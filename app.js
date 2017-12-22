08c531afc95edaf04911d66145b652d88f7e0e2f
Wang1A <32918981+Wang1A@users.noreply.github.com>
2017-12-22 20:18:57 +0800

    task112
    task112

---
+++ app.js
@@ -0,0 +1,13 @@
+const printInventory = require('./main/printInventory');
+let inputs = [
+    'ITEM000001',
+    'ITEM000001',
+    'ITEM000001',
+    'ITEM000001',
+    'ITEM000001',
+    'ITEM000003-2',
+    'ITEM000005',
+    'ITEM000005',
+    'ITEM000005'
+];
+printInventory(inputs);
